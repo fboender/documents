473932ad401aca8b05d2112680b5a2641f567919
todsah <todsah@2b71e653-6815-41dc-8e77-4b9bb3571a7c>
2011-08-07 12:03:58 +0000

    Updated the mk_asciidoc tool. Now creates epub.
    git-svn-id: https://svn.electricmonk.nl/svn/documents/trunk@32 2b71e653-6815-41dc-8e77-4b9bb3571a7c

--- tools/mk_asciidoc.sh
+++ tools/mk_asciidoc.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-/usr/local/bin/asciidoc -a theme=classy -a toc -a toclevels=3 -a numbered $1
-/usr/local/bin/asciidoc -a toc -a toclevels=3 -a numbered -a imagesdir=`pwd`/ -b docbook $1
+asciidoc -a theme=classy -a toc -a toclevels=3 -a numbered $1
+asciidoc -a toc -a toclevels=3 -a numbered -a imagesdir=`pwd`/ -b docbook $1
 
 # Clean up Docbook source
 DBFILE=`basename $1 .txt`.xml
-TMPFILE=`tempfile -d ./`
+db2pdf $DBFILE > /dev/null
+#TMPFILE=`tempfile -d ./`
+#
+#sed -e "/<\/*mediaobject>/ d" -e "/<\/*imageobject>/d" -e "/<textobject>/d" -e "s/imagedata/graphic/" $DBFILE > $TMPFILE
+#mv $TMPFILE $DBFILE
 
-sed -e "/<\/*mediaobject>/ d" -e "/<\/*imageobject>/d" -e "/<textobject>/d" -e "s/imagedata/graphic/" $DBFILE > $TMPFILE
-mv $TMPFILE $DBFILE
+a2x -f epub --stylesheet=/etc/asciidoc/stylesheets/classy.css $1
+#a2x -f pdf --stylesheet=/etc/asciidoc/stylesheets/classy.css $1
