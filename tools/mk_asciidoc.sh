60b2691002883f096207831f0855b213055f2238
todsah <todsah@2b71e653-6815-41dc-8e77-4b9bb3571a7c>
2011-08-13 12:09:36 +0000

    Update to mk_asciidoc tool.
    git-svn-id: https://svn.electricmonk.nl/svn/documents/trunk@38 2b71e653-6815-41dc-8e77-4b9bb3571a7c

--- tools/mk_asciidoc.sh
+++ tools/mk_asciidoc.sh
@@ -2,14 +2,7 @@
 
 asciidoc -a theme=classy -a toc -a toclevels=3 -a numbered $1
 asciidoc -a toc -a toclevels=3 -a numbered -a imagesdir=`pwd`/ -b docbook $1
-
-# Clean up Docbook source
 DBFILE=`basename $1 .txt`.xml
+sed -i -e "/<\/*mediaobject>/ d" -e "/<\/*imageobject>/d" -e "/<textobject>/d" -e "s/imagedata/graphic/" $DBFILE
 db2pdf $DBFILE > /dev/null
-#TMPFILE=`tempfile -d ./`
-#
-#sed -e "/<\/*mediaobject>/ d" -e "/<\/*imageobject>/d" -e "/<textobject>/d" -e "s/imagedata/graphic/" $DBFILE > $TMPFILE
-#mv $TMPFILE $DBFILE
-
 a2x -f epub --stylesheet=/etc/asciidoc/stylesheets/classy.css $1
-#a2x -f pdf --stylesheet=/etc/asciidoc/stylesheets/classy.css $1
