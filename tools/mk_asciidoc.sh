e2197c622282c8212b7114a6f4653620de3172aa
Ferry Boender <ferry.boender@electricmonk.nl>
2017-02-19 16:44:53 +0100

    Make sure mk_asciidoc.sh works properly from different dirs.

--- tools/mk_asciidoc.sh
+++ tools/mk_asciidoc.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-asciidoc -a theme=classy -a toc -a toclevels=3 -a numbered $1
+BASEDIR=$(readlink -f $(dirname $0)/..)
+
+# Generate HTML
+asciidoc -a theme=classy -a toc -a toclevels=3 -a numbered -b html5 $1
+
+# Generate Docbook xml
 asciidoc -a toc -a toclevels=3 -a numbered -a imagesdir=`pwd`/ -b docbook $1
+
+# Fix docbook and generate PDF
 DBFILE=`basename $1 .txt`.xml
 sed -i -e "/<\/*mediaobject>/ d" -e "/<\/*imageobject>/d" -e "/<textobject>/d" -e "s/imagedata/graphic/" $DBFILE
 db2pdf $DBFILE > /dev/null
-a2x -f epub --stylesheet=/etc/asciidoc/stylesheets/classy.css $1
+
+# Generate Epub
+#a2x -f epub --stylesheet=/etc/asciidoc/stylesheets/classy.css $1
+
+cp $BASEDIR/style/asciidoc/classy.css $(dirname $1)
+a2x -f epub --stylesheet=classy.css $1
+rm $(dirname $1)/classy.css
